a2176dee5f1aab72b7016d5f17cb9de6a10b2d5e
hasib4 <hasib4noor@gmail.com>
2022-08-28 10:06:45 +0600

    Updates

---
+++ precache-manifest.d5c4c321236c922d147b30ee290c7b9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c742f8833e793c17fc61",
+    "url": "/robotfriends/static/css/main.55ebc2f0.chunk.css"
+  },
+  {
+    "revision": "c742f8833e793c17fc61",
+    "url": "/robotfriends/static/js/main.c742f883.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robotfriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "aad497554614cfdb2c6e",
+    "url": "/robotfriends/static/css/2.2f4428bf.chunk.css"
+  },
+  {
+    "revision": "aad497554614cfdb2c6e",
+    "url": "/robotfriends/static/js/2.aad49755.chunk.js"
+  },
+  {
+    "revision": "e51a43ac8f47f9940acc",
+    "url": "/robotfriends/static/js/runtime~main.e51a43ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0a4672b482de71c5f386e4592540f226",
+    "url": "/robotfriends/index.html"
+  }
+];
